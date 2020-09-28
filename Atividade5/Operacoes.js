443a9efb9eb22d8904b3711f1bc202c0a9636874
JulioAugusto9 <56029778+JulioAugusto9@users.noreply.github.com>
2020-09-28 12:04:25 -0300

    Add files via upload

---
+++ Atividade5/Operacoes.js
@@ -0,0 +1,8 @@
+var num1 = parseFloat(prompt("Digite o primeiro número:"));
+var num2 = parseFloat(prompt("Digite o segundo número:"));
+
+alert('Soma dos dois = ' + (num1 + num2));
+alert('Subtração do primeiro pelo segundo: ' + (num1 - num2));
+alert('Produto dos dois: ' + (num1 * num2));
+alert('Divisão do primeiro pelo segundo: ' + (num1/num2));
+alert('Resto da divisão do primeiro pelo segundo: ' + (num1 % num2));
